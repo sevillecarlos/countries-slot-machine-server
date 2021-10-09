e6691fe02c5a9824d7fa5522d1a7625cd7a33db4
Carlos Sevilla <sevillecarlos@gmail.com>
2021-10-09 00:07:36 -0600

    fix the issue of country api

--- src/routes/api/v1/countries.ts
+++ src/routes/api/v1/countries.ts
@@ -6,24 +6,29 @@ const routes = express.Router();
 const URL_API = "http://api.countrylayer.com/v2";
 
 routes.post("/country", (req, res) => {
-  http.get(
-    `${URL_API}/name/${req}?access_key=${process.env.ACCESS_TOKEN}&FullText=true`,
-    (resp) => {
-      let data = "";
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-      resp.on("end", () => {
-        const parseData = JSON.parse(data);
-        res.send(parseData);
-      });
-    }
-  );
+  const { countryQuery } = req.body;
+  try {
+    http.get(
+      `${URL_API}/name/${countryQuery}?access_key=3b404a2b4c0ec7cb384617065d0d654b&FullText=true`,
+      (resp) => {
+        let data = "";
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+        resp.on("end", () => {
+          const [parseData] = JSON.parse(data);
+          res.json(parseData);
+        });
+      }
+    );
+  } catch (error) {
+    res.json({ error });
+  }
 });
 
-routes.get("/countries", (req, res) => {
+routes.get("/countries", (_, res) => {
   http.get(
-    `${URL_API}/all?access_key=${process.env.ACCESS_TOKEN}&FullText=true`,
+    `${URL_API}/all?access_key=3b404a2b4c0ec7cb384617065d0d654b&FullText=true`,
     (resp) => {
       let data = "";
       resp.on("data", (chunk) => {
@@ -31,7 +36,7 @@ routes.get("/countries", (req, res) => {
       });
       resp.on("end", () => {
         const parseData = JSON.parse(data);
-        res.send(parseData);
+        res.json(parseData);
       });
     }
   );
@@ -52,7 +57,7 @@ routes.post("/countries", (req, res) => {
           (country: { name: string }) =>
             country.name.toLocaleLowerCase().indexOf(countryQuery) !== -1
         );
-        res.send(filterCountries);
+        res.json(filterCountries);
       });
     }
   );
