122ab084813576c189d2d6a6e417c884d31b7a1d
Carlos Sevilla <sevillecarlos@gmail.com>
2021-10-13 17:55:24 -0600

    set exception to filter countries

--- src/controllers/countries.ts
+++ src/controllers/countries.ts
@@ -19,33 +19,36 @@ const findCountryByName = async (req: Request, res: Response) => {
     // get a specific country by given name
     https.get(`${URL_API}/name/${countryQueryName}`, (resp: any) => {
       let data: string = "";
+      let parseData: any = "";
 
       resp.on("data", (chunk: string) => {
         data += chunk;
       });
       resp.on("end", () => {
-        const parseData = JSON.parse(data);
-        if (parseData?.status !== 404) {
-          const [country]: [Countries] = parseData;
-          //parse the country data to be send
-          const {
-            name: { common: countryName },
-            capital,
-            region,
-            flag,
-          } = country;
-
-          res.status(200).json({
-            countryName,
-            capital,
-            region,
-            flag,
-          });
-        } else {
-          throw new Error("The country don't exist");
-        }
+        parseData = JSON.parse(data);
       });
+
+      if (parseData?.status !== 404) {
+        const [country]: [Countries] = parseData;
+        // parse the country data to be send
+        const {
+          name: { common: countryName },
+          capital,
+          region,
+          flag,
+        } = country;
+
+        res.status(200).json({
+          countryName,
+          capital,
+          region,
+          flag,
+        });
+      } else {
+        throw new Error("Country");
+      }
     });
+    // throw Error("The country don't exist");
   } catch (error) {
     res.status(500).json({ error });
   }
@@ -55,22 +58,26 @@ const findCountryByName = async (req: Request, res: Response) => {
 const getAllCountries = async (_: Request, res: Response) => {
   try {
     // get all countries
-    https.get(`${URL_API}/all/`, (resp) => {
-      let data: string = "";
+    https.get(`${URL_API}/all/`, async (resp) => {
+      const buffers = [];
+      for await (const chunk of resp) {
+        buffers.push(chunk);
+      }
+      const data = Buffer.concat(buffers).toString();
 
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-      resp.on("end", () => {
-        const parseData: [Countries] = JSON.parse(data);
-        const infoCountries = parseData.map((country: any) => ({
-          countryName: country.name.common,
-          capital: country.capital,
-          region: country.region,
-          flag: country.flag,
-        }));
-        res.status(200).json(infoCountries);
-      });
+      // parse to JSON the string data
+      const parseData: [Countries] = JSON.parse(data);
+
+      // parse the data to send
+      const infoCountries = parseData.map((country: any) => ({
+        countryName: country.name.common,
+        capital: country.capital,
+        region: country.region,
+        flag: country.flag,
+      }));
+
+      res.send(infoCountries);
+      res.end();
     });
   } catch (error) {
     throw error.message;
@@ -81,46 +88,58 @@ const getListCountriesByName = (req: Request, res: Response) => {
   // get the query from body
   const countryQuery: string = req.body.countryQuery;
   // verify if are error when validate the request body
-  const errors = validationResult(req);
+  const validationError = validationResult(req);
   // send error message if don't pass validation
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+  if (!validationError.isEmpty()) {
+    return res.status(400).json({ errors: validationError.array() });
   }
+  // find the list of country that match the query
+  https.get(`${URL_API}/all`, async (resp) => {
+    const { statusCode } = res;
 
-  try {
-    // find the list of country that match the query
-    https.get(`${URL_API}/all`, (resp) => {
-      let data: string = "";
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-      resp.on("end", () => {
-        const parseData: [Countries] = JSON.parse(data);
+    let error;
 
-        //filter the countries that match the given query
-        const filterCountries = parseData.filter(
-          (country: { name: { common: string } }) =>
-            country.name.common.toLowerCase().indexOf(countryQuery) !== -1
-        );
-        //check a filter country
-        if (filterCountries.length !== 0) {
-          //parse the country data to be send
-          const infoCountries = filterCountries.map((country: any) => ({
-            countryName: country.name.common,
-            capital: country.capital,
-            region: country.region,
-            flag: country.flag,
-          }));
+    if (statusCode !== 200) {
+      error = new Error(`Failed to make the request,
+      Status Code: ${statusCode}`);
+    }
 
-          res.status(200).json(infoCountries);
-        } else {
-          throw new Error("The country don't exist");
-        }
-      });
-    });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+    if (error) {
+      res.status(statusCode).json({ error: error.message });
+    }
+
+    const buffers = [];
+    for await (const chunk of resp) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+
+    // parse to JSON the string data
+    const parseData: [Countries] = JSON.parse(data);
+    
+    // filter the countries that match the given query
+    const filterCountries = parseData.filter(
+      (country: { name: { common: string } }) =>
+        country.name.common.toLowerCase().indexOf(countryQuery) !== -1
+    );
+
+    // check a filters countries
+    if (filterCountries.length !== 0) {
+      // parse the country data to be send
+      const infoCountries = filterCountries.map((country: any) => ({
+        countryName: country.name.common,
+        capital: country.capital,
+        region: country.region,
+        flag: country.flag,
+      }));
+
+      res.status(200).json(infoCountries);
+    } else {
+      res.status(500).json({ reason: "The country don't exist" });
+    }
+
+    res.end();
+  });
 };
 
 export default { findCountryByName, getAllCountries, getListCountriesByName };
--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,30 @@ import express from "express";
 import countriesRoute from "./routes/api/countries";
 import dotenv from "dotenv";
 import cors from "cors";
-
+// configure the ENVIROMENT VARIABLE
 dotenv.config();
 
 const app = express();
+// recognize the incoming request object
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
-
+// accept the cross origin resource
 app.use(cors());
 
 app.get("/", (_, res) => {
   res.send("Server Up");
 });
 
+// set routes to the server
 app.use(countriesRoute);
 
+// handle route error
 app.use((_, res) => {
-  const error = new Error("Not found");
+  const error = new Error("Route not found");
   return res.status(404).json({
     message: error.message,
   });
 });
-
+// handle route error
 app.listen(PORT);
